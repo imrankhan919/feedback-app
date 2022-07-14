7906d9fcb75b54a370b1e71da6e480654e5498fe
imrankhan919 <imrankhan919.ik@gmail.com>
2022-07-14 15:50:06 +0530

    day8

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -5,112 +5,102 @@ function RatingSelect() {
     const [selected , setSelected] = useState(10)
     
     const handleChange = (e) =>{
-
+       setSelected(+e.target.value);
     }
 
   return (
-   <ul className="rating">
+    <ul className="rating">
     <li>
         <input type="radio"
         id='num1'
-        name = 'rating'
-        value = "1"
-        onChange={handleChange}
-        checked = {selected === 1 }
-        />
-        <label htmlFor="'num1">1</label>
+        name='rating'
+        value= '1'
+        onChange = {handleChange}
+        checked = {selected === 1} />
+        <label htmlFor="num1">1</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "2"
-        onChange={handleChange}
-        checked = {selected === 2 }
-        />
-        <label htmlFor="'num1">2</label>
+        id='num2'
+        name='rating'
+        value= '2'
+        onChange = {handleChange}
+        checked = {selected === 2} />
+        <label htmlFor="num2">2</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "3"
-        onChange={handleChange}
-        checked = {selected === 3 }
-        />
-        <label htmlFor="'num1">3</label>
+        id='num3'
+        name='rating'
+        value= '3'
+        onChange = {handleChange}
+        checked = {selected === 3} />
+        <label htmlFor="num3">3</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "4"
-        onChange={handleChange}
-        checked = {selected === 4 }
-        />
-        <label htmlFor="'num1">4</label>
+        id='num4'
+        name='rating'
+        value= '4'
+        onChange = {handleChange}
+        checked = {selected === 4} />
+        <label htmlFor="num4">4</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "5"
-        onChange={handleChange}
-        checked = {selected === 5 }
-        />
-        <label htmlFor="'num1">5</label>
+        id='num5'
+        name='rating'
+        value= '5'
+        onChange = {handleChange}
+        checked = {selected === 5} />
+        <label htmlFor="num5">5</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "6"
-        onChange={handleChange}
-        checked = {selected === 6 }
-        />
-        <label htmlFor="'num1">6</label>
+        id='num6'
+        name='rating'
+        value= '6'
+        onChange = {handleChange}
+        checked = {selected === 6} />
+        <label htmlFor="num6">6</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "7"
-        onChange={handleChange}
-        checked = {selected === 7 }
-        />
-        <label htmlFor="'num1">7</label>
+        id='num7'
+        name='rating'
+        value= '7'
+        onChange = {handleChange}
+        checked = {selected === 7} />
+        <label htmlFor="num7">7</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "8"
-        onChange={handleChange}
-        checked = {selected === 8 }
-        />
-        <label htmlFor="'num1">8</label>
+        id='num8'
+        name='rating'
+        value= '8'
+        onChange = {handleChange}
+        checked = {selected === 8} />
+        <label htmlFor="num8">8</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "9"
-        onChange={handleChange}
-        checked = {selected === 9 }
-        />
-        <label htmlFor="'num1">9</label>
+        id='num9'
+        name='rating'
+        value= '9'
+        onChange = {handleChange}
+        checked = {selected === 9} />
+        <label htmlFor="num9">9</label>
     </li>
     <li>
         <input type="radio"
-        id='num1'
-        name = 'rating'
-        value = "10"
-        onChange={handleChange}
-        checked = {selected === 10 }
-        />
-        <label htmlFor="'num1">10</label>
+        id='num10'
+        name='rating'
+        value= '10'
+        onChange = {handleChange}
+        checked = {selected === 10} />
+        <label htmlFor="num10">10</label>
     </li>
-   </ul>
+</ul>
   )
 }
 
